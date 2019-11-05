8aa3c6306e37b204ec0bb44c5ea8fd5aeecb0a34
Akimbaev <30404027+akimbaev@users.noreply.github.com>
2019-11-06 00:11:10 +0600

    upd changing main page and models

--- djangoproject/MySite/Cinema/models.py
+++ djangoproject/MySite/Cinema/models.py
@@ -4,5 +4,6 @@ class Articles(models.Model):
     title=models.CharField(max_length=120)
     post=models.TextField()
     date=models.DateTimeField()
+    rating=models.CharField(max_length=120)
     def __str__(self):
         return self.title
