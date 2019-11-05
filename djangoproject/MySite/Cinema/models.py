3788475978f87b6ed0d2cd87959f423c351ddefa
Broomhilda Von Shaft <30404027+akimbaev@users.noreply.github.com>
2019-11-05 22:56:30 +0600

    up

--- djangoproject/MySite/Cinema/models.py
+++ djangoproject/MySite/Cinema/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Articles(models.Model):
+    title=models.CharField(max_length=120)
+    post=models.TextField()
+    date=models.DateTimeField()
+    def __str__(self):
+        return self.title
