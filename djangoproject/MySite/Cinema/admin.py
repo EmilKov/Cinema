e349f57d5ff95775f3c021f6a3bf830b1f955521
Billie Crash <dj.dragon2401@gmail.com>
2019-11-27 21:04:42 +0600

    Revert "GG"
    This reverts commit 49e349dcfbdf4c9089693ecc4277e4131eca82fd.

--- djangoproject/MySite/Cinema/admin.py
+++ djangoproject/MySite/Cinema/admin.py
@@ -2,27 +2,4 @@ from django.contrib import admin
 from Cinema.models import *
 # Register your models here.
 admin.site.register(Articles)
-from Cinema.models import Profile
-
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
-
-class ProfileInline(admin.StackedInline):
-    model = Profile
-    can_delete = False
-    verbose_name_plural = 'Profile'
-    fk_name = 'user'
-
-class CustomUserAdmin(UserAdmin):
-    inlines = (ProfileInline, )
-
-    def get_inline_instances(self, request, obj=None):
-        if not obj:
-            return list()
-        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
-
-
-admin.site.unregister(User)
-admin.site.register(User, CustomUserAdmin)
-
-# admin.site.register(ExampleModel)
+admin.site.register(user)
