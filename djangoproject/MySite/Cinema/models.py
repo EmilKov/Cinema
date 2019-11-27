95b932030580eb23300990fa5192a4d43a721b5d
Billie Crash <dj.dragon2401@gmail.com>
2019-11-27 21:05:27 +0600

    Revert "Revert "GG""
    This reverts commit e349f57d5ff95775f3c021f6a3bf830b1f955521.

--- djangoproject/MySite/Cinema/models.py
+++ djangoproject/MySite/Cinema/models.py
@@ -1,18 +1,36 @@
 from django.db import models
 from django.core.validators import RegexValidator
 
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
-class user(models.Model):
-    unic_user_id=models.CharField(max_length=120)
-    username=models.TextField()
-    password=models.TextField()
-    mail=models.TextField()
-    date_of_reg=models.DateTimeField(auto_now_add=True)
-    rating_of_user=models.CharField(max_length=120)
-    avatarka = models.ImageField(upload_to='image_user/')
+from django.dispatch import receiver
+# class ExampleModel(models.Model):
+#     model_pic = models.ImageField(upload_to = 'profile_image2')
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.PROTECT)
+    description = models.CharField(max_length=100, default='')
+    city = models.CharField(max_length=100, default='')
+    website = models.URLField(default='')
+    phone = models.IntegerField(default=0)
+    image = models.ImageField(upload_to='profile_image', blank=True)
+    objects = models.Manager()
+
     def __str__(self):
-        return self.username
+        return self.user.username
 
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
+
+# def create_profile(sender, **kwargs):
+#     if kwargs['created']:
+#         user_profile = UserProfile.objects.create(user=kwargs['instance'])
+#
+# post_save.connect(create_user_profile, sender=User)
 class Articles(models.Model):
     title=models.CharField(max_length=120)
     post=models.TextField()
