26795a103027adb0e6db2039b2dceb00607fd36a
Akimbaev <30404027+akimbaev@users.noreply.github.com>
2019-11-29 02:00:07 +0600

    A lot of update of list and main page

--- djangoproject/MySite/Cinema/urls.py
+++ djangoproject/MySite/Cinema/urls.py
@@ -16,23 +16,21 @@ urlpatterns = [
     path("movielist/", views.movielist, name='movielist'),
     # path("moviesingle/<int:pk>/".views.moviesingle,name='moviesingle'),
     path("moviesingle/<slug:movie_id>/", views.moviesingle, name='moviesingle'), #work
-    # path("moviesingle1/<slug:movie_id>/", views.moviesingle, name='moviesingle'), #work
     # path("user/",views.)
     path('user/', views.user, name='user'),
     path('movie/<int:pk>/',
     DetailView.as_view(model=Movie,
     template_name="Cinema/moviesingle.html")),
-    # path('movie_all/<int:pk>/)', views.whole_list, {'model': models.Articles}, name='whole_list'),
     url(r'^movie_all/(?P<page>\d*)', views.whole_list, {'model': models.Movie}, name='whole_list'),
-    # path('register/', views.register,name='register'),
-    # path('account/', include('django.contrib.auth.urls')),
     path('register/',views.RegisterFormView.as_view(),name='signup'),
     path('login/',views.LoginFormView.as_view(),name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('accounts/', TemplateView.as_view(template_name="logins/index.html")),
     path('search/', views.search, name='search'),
     path('news/', views.news, name='news'),
-    url(r'^recommendations2/(?P<page>\d*)', views.get_suggests, {'model': models.Movie}, name='get_suggests'),
+    # path('moviegrid/', views.moviegrid, name='moviegrid'),
+    url(r'^moviegrid/(?P<page>\d*)', views.whole_list_grid, {'model': models.Movie}, name='whole_list_grid'),
+    url(r'^recommendations/(?P<page>\d*)', views.get_suggests, {'model': models.Movie}, name='get_suggests'),
     url(r'^recommendation/decade/(?P<page>\d*)', views.get_rec, {'model': models.Movie}, name='get_rec'),
     url(r'^worstfilms/(?P<page>\d*)', views.get_worstfilms, {'model': models.Movie}, name='get_worstfilms'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- djangoproject/MySite/Cinema/views.py
+++ djangoproject/MySite/Cinema/views.py
@@ -57,6 +57,28 @@ def whole_list(request, model, page):
     data = {'items': objects[10 * (page - 1):last_item_index], 'current_page': page, 'page_number': total_page,
             'pages': pages}
     return render(request, 'Cinema/{}list2.html'.format(model.get_name()), data)
+def whole_list_grid(request, model, page):
+    if page == '':
+        page = 1
+    # if page :
+    #     return render(request, 'Cinema/404.html')
+    page = int(page)
+    objects = model.objects.all()
+
+    total_page = int(ceil(len(objects) / 12))
+    if page > total_page:
+        return render(request, 'Cinema/error404.html')
+    last_item_index = 24 * page if page != total_page else len(objects)
+    pages = []
+    end_distance = total_page - page
+    start_page_num = page - 5 if end_distance >= 5 else page - 10 + end_distance
+    end_page_num = page + 5 if page > 5 else 10
+    for i in range(start_page_num, end_page_num + 1):
+        if 1 <= i <= total_page:
+            pages.append(i)
+    data = {'items': objects[24 * (page - 1):last_item_index], 'current_page': page, 'page_number': total_page,
+            'pages': pages}
+    return render(request, 'Cinema/{}grid.html'.format(model.get_name()), data)
 
 def get_suggests(request, model, page):
     if page == '':
@@ -131,7 +153,8 @@ def get_worstfilms(request,model,page):
 
 def index(request):
     return render(request, 'Cinema/index.html')
-
+def moviegrid(request):
+    return render(request, 'Cinema/moviegrid.html')
 
 def news(request):
     return render(request, 'Cinema/bloglist.html')
