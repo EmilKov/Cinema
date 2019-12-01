d4f140dfa1fffdc7673f18de988c35e1f37ac0a2
Django Unchained <56864444+SanzharM@users.noreply.github.com>
2019-12-01 17:34:26 +0600

    schedule sector
    html + Cinema/views.py

--- djangoproject/MySite/Cinema/views.py
+++ djangoproject/MySite/Cinema/views.py
@@ -166,6 +166,116 @@ def moviesingle(request, movie_id):
     movie_list = Movie.objects.get(pk=movie_id)
     return render(request, 'Cinema/moviesingle1.html', {'movie_list': movie_list})
 
+def kinoparkalmaty(request):
+    movie_theatre = ['KINOPARK 5 Atakent','KINOPARK 6 Sputnik','KINOPARK 8 Moskva',
+                     'KINOPARK 11 IMAX Esentai','KINOPARK 16 Forum',]
+
+    movie_theatre_name = {}
+    movie_theatre_id = ['163','72','149','99','167']
+    for j in range(len(movie_theatre)):
+        movie_theatre_name.update({movie_theatre[j]: movie_theatre_id[j]})
+
+    movie_theatre_info = [{'name':'KINOPARK 5 Atakent','address':'г. Алматы, Atakent Mall, ул. Тимирязева 42','phone_number':'+7 702 481 18 90',
+                           'photo':'http://www.kinopark.kz/storage/app/uploads/public/5cf/4c3/be5/5cf4c3be54dd1967595125.jpg',
+                           'url':'https://kino.kz/cinema/163'},
+
+                          {'name':'KINOPARK 6 Sputnik', 'address': 'г. Алматы, мкр. Мамыр-1, 8А, ТРК "Спутник", 3 этаж', 'phone_number': '8 (701) 767-46-03',
+                           'photo':'https://s.kino.kz/gallery/cinema/72/p223x267.jpg',
+                           'url': 'https://s.kino.kz/cinema/72'},
+
+                          {'name':'KINOPARK 8 Moskva', 'address': 'г. Алматы, пр. Абая, уг. пр. Алтынсарина, ТРЦ MOSKVA Metropolitan', 'phone_number': '+7 778 099 09 17',
+                           'photo':'http://www.kinopark.kz/storage/app/uploads/public/5b0/3ee/d53/5b03eed530f25658777945.jpg',
+                           'url':'https://s.kino.kz/cinema/149'},
+
+                          {'name':'KINOPARK 11 IMAX Esentai', 'address': 'г. Алматы, Esentai Mall, 4 этаж, пр. Аль-Фараби, 77/8', 'phone_number': '+7 701 762 45 11',
+                           'photo':'http://www.kinopark.kz/storage/app/uploads/public/5a1/fa6/694/5a1fa66943e9d114540849.jpg',
+                           'url':'https://s.kino.kz/cinema/99'},
+
+                          {'name':'KINOPARK 16 Forum', 'address': 'г. Алматы, пр. Сейфуллина 617, 5 этаж', 'phone_number': '+7 705 208 9595',
+                           'photo':'http://www.kinopark.kz/storage/app/uploads/public/5d9/33a/574/5d933a5740dbf291927498.jpg',
+                           'url':'https://s.kino.kz/cinema/167'},
+                          ]
+
+    data = {}
+    if len(movie_theatre_info) == len(movie_theatre):
+        for i in range(len(movie_theatre)):
+            data.update({movie_theatre_id[i]: movie_theatre_info[i]})
+    print(data)
+    newdata = {}
+    newdata.update({'id':data})
+    print('\n\n\n\n',newdata,'\n\n\n\n\n')
+    return render(request, 'Cinema/kinopark.html', data)
+
+
+def schedule(request):
+    return render(request,'Cinema/schedule.html')
+
+
+def kinoparkastana(request):
+    movie_theatre = ['KINOPARK 6 Keruencity', 'KINOPARK 7 IMAX Keruen', 'KINOPARK 9 Saryarqa']
+    movie_theatre_id = ['62', '71', '63']
+
+    movie_theatre_info = [
+        {'name':'KINOPARK 6 Keruencity','address': 'ТРЦ «KeruenCity Астана»,3 этаж,г.Нур-Султан,Коргальджинское шоссе,1',
+         'phone_number': '+7 (701) 870-70-01', 'photo': 'http://www.kinopark.kz/storage/app/uploads/public/5a1/406/e54/5a1406e5460d3275085249.jpg',
+         'url': 'https://kino.kz/cinema/62'},
+
+        {'name': 'KINOPARK 7 IMAX Keruen', 'address': 'ТРЦ «Keruen», 4 этаж, г. Нур-Cултан, ул. Достык, 9',
+         'phone_number': '+7 (701) 870-70-02', 'photo': 'http://www.kinopark.kz/storage/app/uploads/public/5b0/3ef/58b/5b03ef58be00a676602909.jpg',
+         'url': 'https://kino.kz/cinema/71'},
+
+        {'name': 'KINOPARK 9 Saryarqa', 'address': 'ТРЦ «Saryarqa», 3 этаж, г. Нур-Султан, ул. Туран, 24',
+         'phone_number': ' +7 (701) 555-73-70', 'photo': 'http://www.kinopark.kz/storage/app/uploads/public/5b0/3ee/6a7/5b03ee6a7410a906012682.jpg',
+         'url': 'https://kino.kz/cinema/63'},
+
+    ]
+    data = {}
+    if len(movie_theatre_info) == len(movie_theatre):
+        for i in range(len(movie_theatre)):
+            data.update({movie_theatre_id[i]: movie_theatre_info[i]})
+    print(data)
+    return render(request, 'Cinema/kinoparkastana.html', data)
+
+
+def kinoparkshymkent(request):
+    movie_theatre = ['KINOPARK 5 Mega Planet', 'KINOPARK 5 Hyper House', 'KINOPARK 4 Nauryz Park']
+    movie_theatre_info = [
+        {'name': 'KINOPARK 5 Mega Planet', 'address': 'ТРЦ «Mega Planet», 3 этаж, г. Шымкент, проспект Тауке-Хана',
+         'phone_number': '+7 (725) 2301-335', 'photo': 'http://www.kinopark.kz/storage/app/uploads/public/5a1/406/d1c/5a1406d1c4847291623922.jpg',
+         'url': 'https://kino.kz/cinema/61'},
+
+        {'name': 'KINOPARK 5 Hyper House', 'address': 'ТРЦ «Hyper House», 3-й этаж, г. Шымкент, ул. Дулати, 200А',
+         'phone_number': '+7 (775) 406 6884', 'photo': 'http://www.kinopark.kz/storage/app/uploads/public/5cc/d35/3b6/5ccd353b62f15572833181.jpg',
+         'url': 'https://kino.kz/cinema/161'},
+
+        {'name': 'KINOPARK 4 Nauryz Park', 'address': 'г. Шымкент, пр. Байдибек Би 25, 2 этаж',
+         'phone_number': '+7 778 636 7741', 'photo': 'http://www.kinopark.kz/storage/app/uploads/public/5d9/1ee/7f5/5d91ee7f5760e541227844.jpg',
+         'url': 'https://kino.kz/cinema/170'},
+
+    ]
+    movie_theatre_id = ['61', '161', '170']
+
+    data = {}
+    if len(movie_theatre_info) == len(movie_theatre):
+        for i in range(len(movie_theatre_info)):
+            data.update({movie_theatre_id[i]:movie_theatre_info[i]})
+    return render(request, 'Cinema/kinoparkshymkent.html', data)
+
+
+def kinoparkaktobe(request):
+    movie_theatre = ['KINOPARK 7 Keruencity Aktobe']
+    movie_theatre_info = [
+        {'name': 'KINOPARK 7 Keruencity Aktobe', 'address': 'г. Актобе, ул М. Маметовой 4, ТРЦ "Mega-Aktobe", 3-й этаж',
+         'phone_number': '+7 701 767 46 02', 'photo': 'https://s.kino.kz/gallery/cinema/73/p223x267.jpg',
+         'url': 'https://kino.kz/cinema/73'},
+    ]
+    movie_theatre_id = ['73']
+    data = {}
+    if len(movie_theatre) == len(movie_theatre_info):
+        for i in range(len(movie_theatre)):
+            data.update({movie_theatre_id[i]: movie_theatre_info[i]})
+    return render(request,'Cinema/kinoparkaktobe.html', data)
+
 
 def user(request):
     return render(request, 'registration/userprofile.html')
